5469eb5575d0d0883b2977cbf6404c7f8a22ce0a
Tommaso Ciarlariello <tomasciarlariello@gmail.com>
2021-12-27 22:36:57 -0800

    Add brainScrape() function to scrape Brainscape; fix bugs in local storage deletion

--- script.js
+++ script.js
@@ -1,8 +1,94 @@
-const CORS_API_URL = "https://cors-anywhere.herokuapp.com/";
-const SAMPLE_URL =
-  "https://www.brainscape.com/flashcards/atomic-structure-8592328/packs/15958964";
+// Select each element by id
+const template = document.getElementById("list-item-template");
+const list = document.getElementById("list");
+const questionInput = document.getElementById("question");
+const answerInput = document.getElementById("answer");
+const itemForm = document.getElementById("item-form");
+const numberForm = document.getElementById("number-form");
+const addButton = document.getElementById("add-button");
+const quizInput = document.getElementById("quiz-input");
+const quizButton = document.getElementById("quiz-button");
+const quizForm = document.getElementById("quiz-form");
 
-function getSource(url) {
+// Define constants for local storage
+const LOCAL_STORAGE_PREFIX = "CARRIER_GOOSE";
+const ITEM_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-items`;
+
+// Initialize array of items & render them
+let items = loadItems();
+items.forEach(renderItems);
+
+// Create new item when question & answer form is submitted
+addButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (questionInput.value === "" || answerInput.value === "") return;
+
+  // Template for new list item
+  const newItem = {
+    question: questionInput.value,
+    answer: answerInput.value,
+    id: new Date().valueOf().toString()
+  };
+
+  // Add the item to the items array and local storage
+  items.push(newItem);
+  renderItems(newItem);
+  saveItems();
+
+  // Clear the question and answer inputs
+  questionInput.value = "";
+  answerInput.value = "";
+});
+
+// Event listener to remove items on click
+list.addEventListener("click", (e) => {
+  if (!e.target.matches("[data-button-delete]")) return;
+
+  const parent = e.target.closest(".list-item");
+  const listId = parent.dataset.listId;
+  parent.remove();
+  items = items.filter((item) => item.id !== listId);
+  saveItems();
+});
+
+// Function to render list of items
+function renderItems(item) {
+  // Create a clone of item template from HTML & assign an Id
+  const templateClone = template.content.cloneNode(true);
+  const listItem = templateClone.querySelector(".list-item");
+  listItem.dataset.listId = item.id;
+
+  // Add the question & answer text to their corresponding span elements
+  const questionElement = templateClone.querySelector(
+    "[data-list-item-question]"
+  );
+  const answerElement = templateClone.querySelector("[data-list-item-answer]");
+  questionElement.innerText = item.question;
+  answerElement.innerText = item.answer;
+
+  // Append the template clone onto the list element
+  list.appendChild(templateClone);
+}
+
+// Function to load items currently stored in local storage
+function loadItems() {
+  const itemString = localStorage.getItem(ITEM_STORAGE_KEY);
+  return JSON.parse(itemString) || [];
+}
+
+// Function to save items in local storage
+function saveItems() {
+  localStorage.setItem(ITEM_STORAGE_KEY, JSON.stringify(items));
+}
+
+// Link for cors proxy
+const CORS_API_URL = "https://cors-anywhere.herokuapp.com/";
+
+// Array waiting for elements of source page to be pushed to it
+const sourceElements = [];
+
+function brainScrape(url) {
   fetch(`${CORS_API_URL}${url}`)
     .then((response) => {
       return response;
@@ -11,146 +97,114 @@ function getSource(url) {
       return data.text();
     })
     .then((text) => {
-      console.log(text);
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(text, "text/html");
+
+      const questions = doc.querySelectorAll(".card-question-text");
+      const answers = doc.querySelectorAll(".card-answer-text");
+
+      let questionPair = [];
+      questions.forEach((pair) => {
+        questionPair.push(pair.innerText);
+      });
+
+      let answerPair = [];
+      answers.forEach((pair) => {
+        answerPair.push(pair.innerText);
+      });
+
+      for (let i = 0; i < questionPair.length; i++) {
+        if (answerPair[i] === null) return;
+
+        sourceElements[i] = {
+          question: questionPair[i],
+          answer: answerPair[i],
+          id: new Date().valueOf().toString() + i
+        };
+      }
     });
 }
-console.log(getSource(SAMPLE_URL));
 
-// // Select each element by id
-// const template = document.getElementById("list-item-template");
-// const list = document.getElementById("list");
-// const questionInput = document.getElementById("question");
-// const answerInput = document.getElementById("answer");
-// const itemForm = document.getElementById("item-form");
-// const numberForm = document.getElementById("number-form");
-// const addButton = document.getElementById("add-button");
-// const quizInput = document.getElementById("quiz-input");
-// const quizButton = document.getElementById("quiz-button");
-// const quizForm = document.getElementById("quiz-form");
+// Function to add Brainscape items to the list
+quizButton.addEventListener("click", (e) => {
+  e.preventDefault();
 
-// // Define constants for local storage
-// const LOCAL_STORAGE_PREFIX = "CARRIER_GOOSE";
-// const ITEM_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-items`;
+  if (quizInput.value === "") return;
+  console.log(quizInput.value);
 
-// // Initialize array of items & render them
-// let items = loadItems();
-// items.forEach(renderItems);
+  brainScrape(quizInput.value);
 
-// // Create new item when question & answer form is submitted
-// addButton.addEventListener("click", (e) => {
-//   e.preventDefault();
+  for (let i = 0; i < sourceElements.length; i++) {
+    console.log(sourceElements[i]);
+    items.push(sourceElements[i]);
+    renderItems(sourceElements[i]);
+    saveItems();
+  }
 
-//   if (questionInput.value === "" || answerInput.value === "") return;
+  console.log(items);
 
-//   const newItem = {
-//     question: questionInput.value,
-//     answer: answerInput.value,
-//     id: new Date().valueOf().toString(),
-//   };
+  // Clear the question and answer inputs
+  quizInput.value = "";
+});
 
-//   // Add the item to the items array and local storage
-//   items.push(newItem);
-//   renderItems(newItem);
-//   saveItems();
+/*
+TODO LIST
 
-//   // Clear the question and answer inputs
-//   questionInput.value = "";
-//   answerInput.value = "";
-// });
+- Twilio API
+- Quiz button glitch
+- Scrolling list (fades into sky)
+- Delete All button
+- Hide answer in a link
+- Format answers to remove "/n" & "/t"
+- Maximum # of flashcards
+- Host cors-anywhere
+- CSS (once we acquire landing page from Fiverr)
+- How does it work?
+- Learn More
+- Host website
+*/
 
-// list.addEventListener("click", (e) => {
-//   if (!e.target.matches("[data-button-delete]")) return;
+/*
+Carrier Goose Delivery! 
+-
+-
+-
+-----------
+Answer
+___________
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+^
+Question:
+What is scarcity?
 
-//   const parent = e.target.closest(".list-item");
-//   const listID = parent.dataset.todoId;
-//   parent.remove();
-//   items = items.filter((item) => item.id !== itemId);
-//   saveItems();
-// });
 
-// // Function to render list of items
-// function renderItems(item) {
-//   // Create a clone of item template from HTML & assign an Id
-//   const templateClone = template.content.cloneNode(true);
-//   const listItem = templateClone.querySelector(".list-item");
-//   listItem.dataset.listId = item.id;
 
-//   // Add the question & answer text to their corresponding span elements
-//   const questionElement = templateClone.querySelector(
-//     "[data-list-item-question]"
-//   );
-//   const answerElement = templateClone.querySelector("[data-list-item-answer]");
-//   questionElement.innerText = item.question;
-//   answerElement.innerText = item.answer;
 
-//   // Append the template clone onto the list element
-//   list.appendChild(templateClone);
-// }
-
-// // Function to load items currently stored in local storage
-// function loadItems() {
-//   const itemString = localStorage.getItem(ITEM_STORAGE_KEY);
-//   return JSON.parse(itemString) || [];
-// }
-
-// // Function to save items in local storage
-// function saveItems() {
-//   localStorage.setItem(ITEM_STORAGE_KEY, JSON.stringify(items));
-// }
-
-// // Function to add Brainscape items to the list
-// quizButton.addEventListener("click", (e) => {
-//   e.preventDefault();
-
-//   if (quizInput.value === "") return;
-
-//   console.log(quizInput.value);
-//   // Clear the question and answer inputs
-//   quizInput.value = "";
-// });
-
-// async function scrapeSite(url) {
-//   const browser = await launch({
-//     product: "chrome",
-//     headless: true,
-//   });
-//   const page = await browser.newPage();
-//   await page.goto(url);
-
-//   const grabQuestionAnswer = await page.evaluate(() => {
-//     const questions = document.querySelectorAll(".card-question-text");
-//     const answers = document.querySelectorAll(".card-answer-text");
-//     let pairs = [];
-
-//     let questionPair = [];
-//     questions.forEach((pair) => {
-//       questionPair.push(pair.innerText);
-//     });
-
-//     let answerPair = [];
-//     answers.forEach((pair) => {
-//       answerPair.push(pair.innerText);
-//     });
-
-//     for (let i = 0; i < questionPair.length; i++) {
-//       if (answerPair[i] === null) return;
-
-//       pairs[i] = {
-//         question: questionPair[i],
-//         answer: answerPair[i],
-//         id: new Date().valueOf().toString(),
-//       };
-//     }
-
-//     return pairs;
-//   });
-
-//   console.log(grabQuestionAnswer);
-//   await browser.close();
-// }
-
-// console.log(
-//   scrapeSite(
-//     "https://www.brainscape.com/flashcards/atomic-structure-8592328/packs/15958964"
-//   )
-// );
+*/
