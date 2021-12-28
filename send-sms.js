c96746bd7d84d46e3c403e80a37aeacc9f29f185
Nicole <nchung@live.com>
2021-12-28 00:44:10 -0800

    Add and set up Twilio API call

--- send-sms.js
+++ send-sms.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
+console.log(process.env);
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-const client = require("twilio")(accountSid, authToken);
+const twilio = require("twilio");
+const client = new twilio(accountSid, authToken);
 
 client.messages
   .create({
