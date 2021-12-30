000ed7eb2ae1d569b669fe3354200ac2d9c2b5a6
Tommaso Ciarlariello <tomasciarlariello@gmail.com>
2021-12-30 07:09:23 -0800

    Design for landing page; add minor Twilio functionality; updates to CSS, HTML and JS

---
+++ send-sms.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+const twilio = require("twilio");
+const client = new twilio(accountSid, authToken);
+
+function sendMessage(question, answer, number) {
+  client.messages
+    .create({
+      body:
+        `\nCarrier Goose Delivery!\n\n           🦢✉️📬\n \n` +
+        `A: ${answer}\n\n^\n^\n^\n^\n^\n^\n^\n^\n^\n^\n^\n^\n^` +
+        `\n^\n^\n^\n^\n^\n^\n^\n^\n^\n^\n^\n^\n^\n^\n^\n^\n^\n` +
+        `Q: ${question}\n\nScroll up to see the answer!`,
+
+      to: number,
+      from: "+16479552565"
+    })
+    .then((message) => console.log(message.sid));
+}
