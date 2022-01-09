c903946fd8e06e3745f3d5155863682856ec4ff6
Nicole <nchung@live.com>
2022-01-08 19:19:38 -0500

    Add phone number POST request and sendMessage() function export

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-// Import express from express library
+// Required External Modules
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const ejs = require("ejs");
-
+const { sendMessage } = require("./public/javascripts/send-sms");
+const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
 
 // Configure express application
@@ -16,6 +17,7 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
@@ -24,6 +26,20 @@ const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
+// Routes Definitions
+app
+  .route("/")
+
+  .post((req, res) => {
+    const phoneNumber = req.body.number;
+    console.log(phoneNumber);
+    console.log(req.body);
+    let question = "hi tomas!";
+    let answer = "what's up?";
+    sendMessage(question, answer, phoneNumber);
+  });
+
+// Server Activation
 app.use("/", indexRouter);
 
 app.listen(process.env.PORT || 8000);
